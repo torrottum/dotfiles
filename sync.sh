2c4da5d20c998a5ef6cc28616103f73d9874cea4
Tor Inge Røttum <tor@toringe.no>
2014-03-07 11:02:11 +0100

    fixes #1

--- sync.sh
+++ sync.sh
@@ -6,6 +6,9 @@ GREEN='\033[00;32m'
 CYAN='\033[00;36m'
 RESET='\033[0m'
 
+# Get the dotfiles absoulute path, where it is called from shouldn't matter
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 function sync(){
 	# Some spacing plz!
 	echo ""
@@ -40,10 +43,10 @@ function sync(){
 	echo -e "${CYAN}Checking if dracula-theme is cloned ...${RESET}"
 	if [[ ! -d dracula-theme ]]; then
 		echo "Dracula-theme is not cloned, cloning ..."
-		git clone https://github.com/zenorocha/dracula-theme.git dracula-theme
+		git clone https://github.com/zenorocha/dracula-theme.git $DIR/dracula-theme
 	else
 		echo "Dracula-theme is already cloned. Pulling instead of cloning ..."
-		pushd dracula-theme > /dev/null
+		pushd $DIR/dracula-theme > /dev/null
 		git pull
 		popd > /dev/null
 	fi
@@ -52,11 +55,11 @@ function sync(){
 
 	# I will maybe automate this in the future, but for now, let's just write up each file/folder :)
 	echo -e "${CYAN}Symlinking dotfiles into $HOME ...${RESET}"
-	ln -s $(pwd)/.zshrc $HOME/.zshrc
-	ln -s $(pwd)/.zsh/ $HOME/.zsh
-	ln -s $(pwd)/.brewfile $HOME/.brewfile
-	ln -s $(pwd)/.caskfile $HOME/.caskfile
-	ln -s $(pwd)/.osx $HOME/.osx
+	ln -s $DIR/.zshrc $HOME/.zshrc
+	ln -s $DIR/.zsh/ $HOME/.zsh
+	ln -s $DIR/.brewfile $HOME/.brewfile
+	ln -s $DIR/.caskfile $HOME/.caskfile
+	ln -s $DIR/.osx $HOME/.osx
 
 	echo ""
 
@@ -73,7 +76,7 @@ function sync(){
 
 	echo -e "${CYAN}Setting up colorschemes ...${RESET}"
 	echo "Symlinking dracula-theme for ZSH ..."
-	ln -s $(pwd)/dracula-theme/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+	ln -s $DIR/dracula-theme/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 	# Check if Dracula is already imported, because iTerm will import it multiple times
 	if ! grep -q Dracula $HOME/Library/Preferences/com.googlecode.iterm2.plist; then
 		echo "Importing dracula-theme to iTerm ..."
@@ -88,7 +91,6 @@ function sync(){
 
 echo -e "${CYAN}Dotfiles!${RESET}"
 echo -e "${RED}Warning:${RESET} This might ${RED}**OVERWRITE**${RESET} files in your home directoy!"
-echo -e "Also please note that you should ${RED}never${RESET} run this script outside of the git repo folder\nbad things will probably happen if you do so"
 echo ""
 # TODO: ask
 read -p "Are you sure you want to do this? (y/n) "
