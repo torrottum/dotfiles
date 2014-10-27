5cc2180d88d81c1c11a46b1ee349a142cc2c6dc9
Tor Røttum <tor@rottum.me>
2014-10-27 12:43:06 +0100

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+#git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		  --exclude "README.md" --exclude ".gitmodules" --exclude ".antigen/tests" \
+		  --exclude ".antigen/.git" --exclude ".gitignore" -avh --no-perms . ~;
+
+	echo -e "\nAll done, now run 'source ~/.zshrc'";
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
